2b397d7a2a1984f49abc74f041955baea47ce6b2
jawoon816 <jawoon816@gmail.com>
2021-12-29 01:06:49 +0900

    button수정

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = (props) => {
-  const { bg, children, _onClick, _disabled, margin } = props;
+  const { bg, children, _onClick, _disabled, margin, border } = props;
 
   const styles = {
     bg,
     margin,
+    border,
   };
 
   return (
@@ -19,16 +20,18 @@ const Button = (props) => {
 Button.defaultProps = {
   bg: false,
   margin: "",
+  border: false,
 };
 
 const ButtonWrap = styled.button`
   font-family: "Black Han Sans", serif;
+  font-size: 20px;
   width: 100%;
-  height: 50px;
+  height: 60px;
   color: white;
   border-radius: 10px;
-  border: none;
   ${(props) => (props.bg ? "background:#F1134E;" : "background:black;")}
+  ${(props) => (props.border ? "border: 3px solid #fff;" : "")}
   opacity: ${(props) => (props._disabled ? "0.5" : "1")};
   margin: ${(props) => props.margin};
 `;
