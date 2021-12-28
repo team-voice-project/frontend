7d6c455b68df4d234f8f76d7cb37021f200ff3cc
YYZA <sacoraa@naver.com>
2021-12-28 10:48:06 +0900

    Login expires 수정

--- src/shared/Cookie.js
+++ src/shared/Cookie.js
@@ -19,8 +19,10 @@ const setCookie = (name, value, exp = 5) => {
   let date = new Date();
   // 날짜를 만들어줍니다.
   date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+  let expires = "; expires=" + date.toGMTString();
+  console.log(date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000));
   // 저장!
-  document.cookie = `${name}=${value}; expires=${date.toUTCString()}`;
+  document.cookie = name + "=" + value + expires + "; path=/";
 };
 
 // 만료일을 예전으로 설정해 쿠키를 지웁니다.
