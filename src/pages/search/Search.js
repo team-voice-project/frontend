1ff772115e8e8e07c06505be0a0f57e29f1545a1
jawoon816 <jawoon816@gmail.com>
2022-01-18 17:49:11 +0900

    [fix]css수정

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -56,7 +56,7 @@ const KeywordSearch = (props) => {
 
   useEffect(() => {
     const dispatchValue = () => {
-      const value = inputRef?.current?.value;
+      const value = inputRef.current?.value;
       return value;
     };
     const searchValue = dispatchValue();
@@ -242,6 +242,7 @@ const Multiline = styled.input`
 const Grid = styled.div`
   padding-left: 10px;
   margin-top: 110px;
+  margin-left: 5px;
   /* @media screen and (max-width: 375px) {
     padding-left: 0px;
   } */
