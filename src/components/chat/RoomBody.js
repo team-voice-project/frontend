f091e23dfe1bc57286e844ae30faccf9bf78687a
jawoon816 <jawoon816@gmail.com>
2022-01-20 23:20:58 +0900

    [fix]콘솔삭제

--- src/components/chat/RoomBody.js
+++ src/components/chat/RoomBody.js
@@ -16,14 +16,11 @@ const RoomBody = ({
   setRequestText,
   createRoomId,
 }) => {
-  console.log("[RoomBody] 대화 정보", chat_content);
   const contentScrollRef = useRef(null);
   const [hasMore, setHasMore] = useState(true);
   const [data, setData] = useState([]);
   const [pages, setPages] = useState(2);
   const [load, setLoad] = useState(false);
-  const [scroll_point, setScrollPoint] = useState(null);
-  const topMessageRef = useRef(null);
 
   const totalData = () => {
     const total = [...data, ...chat_content];
@@ -43,44 +40,8 @@ const RoomBody = ({
     contentScrollRef.current.scrollTop = contentScrollRef.current.scrollHeight;
   }, [chat_content]);
 
-  useEffect(() => {
-    // console.log("지금스크롤위치", contentScrollRef.current.scrollTop);
-    // console.log("스크롤 전체", document.documentElement.scrollHeight);
-    // console.log("보이는 영역만큼", document.documentElement.clientHeight);
-    // console.log("가려진 스크롤", document.documentElement.scrollTop);
-    //useState로 페이지의 높이를 담아두기
-    //로딩 된 다음 페이지 높이에서 이전 높이 빼기
-    //그 값을 현재 스크롤바 위치로 설정
-    //페이지1의 스크롤 전체 저장, 페이지2전체스크롤-페이지1전체스크롤
-    // contentScrollRef.current.scrollTo(0, 1200);
-  }, [totalChat]);
-
-  useEffect(() => {
-    // if (scroll_point) {
-    //   contentScrollRef.current.scrollTo(
-    //     0,
-    //     contentScrollRef.current.scrollHeight - scroll_point
-    //   );
-    // }
-    setScrollPoint(contentScrollRef.current.scrollHeight);
-    console.log("스크롤 값=>", scroll_point);
-    //스크롤
-    // console.log(
-    //   "스크롤 결과값",
-    //   contentScrollRef.current.scrollHeight - scroll_point
-    // );
-    // contentScrollRef.current.scrollTo(
-    //   0,
-    //   contentScrollRef.current.scrollHeight - scroll_point
-    // );
-    //
-
-    // setScrollPoint(null);
-  }, [totalChat]);
-
   const _handleReverseScroll = _.throttle((e) => {
     const now_scroll = contentScrollRef.current.scrollTop;
-    console.log("hasMore", hasMore);
     if (now_scroll === 0 && hasMore === true) {
       fetchMoreChatContent();
     }
@@ -116,12 +77,6 @@ const RoomBody = ({
       return;
     }
     const isMe = my_info.userId === message.sendUserId.userId;
-    // console.log(
-    //   "렌더링 될 메시지",
-    //   message,
-    //   my_info.userId,
-    //   message.sendUserId.userId
-    // );
 
     if (isMe) {
       return <SenderBubble message={message} key={`chat-bubble-${i}`} />;
